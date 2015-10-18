00c7c32c16922648f1b3e8a0c2e743385ffd8cf9
Tim Simkins <tim.simkins@tandj.net>
2015-10-18 10:47:15 -0400

    Added command line options for comic and date

--- download_comics.py
+++ download_comics.py
@@ -3,16 +3,35 @@ from kermics.download import ComicDownloaderFactory
 import time
 from datetime import datetime
 import random
+import optparse
 
 app.config.from_object('config')
 app.config.from_pyfile('application.cfg', silent=True)
 
 config = app.config.get('COMICS_CONFIG')
 
-datestamp = datetime.now().strftime('%Y%m%d')
+
+# grab arguments
+
+parser = optparse.OptionParser(description='Download comics')
+
+parser.add_option('-c', metavar='comic_name',
+                   help='Short name for comic')
+
+parser.add_option('-d', metavar='comic_date',
+                   help='Comic date (YYYYMMDD). Defaults to current date.',
+                   default=datetime.now().strftime('%Y%m%d'))
+
+(options, args) = parser.parse_args()
+                   
+datestamp = options.d
+comic_name = options.c
 
 # Get comic names
-comics = config.sections()
+if comic_name:
+    comics = [comic_name, ]
+else:
+    comics = config.sections()
 
 # Random order every time
 random.shuffle(comics)
@@ -21,6 +40,7 @@ for s in comics:
     print "Download %s" % s
     z = ComicDownloaderFactory(s, datestamp)
     if z.download():
-        sleep_time = random.randint(10,20)
-        print "Sleeping %d" % sleep_time
-        time.sleep(sleep_time)
+        if not comic_name:
+            sleep_time = random.randint(10,20)
+            print "Sleeping %d" % sleep_time
+            time.sleep(sleep_time)
